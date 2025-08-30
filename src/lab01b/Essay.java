93ab35e143ea4035c94dcc54ed6f7dc2e67da830
Edric1234 <edricjtran@gmail.com>
2025-08-30 14:21:05 -0400

    Full implementation of Essay Class

--- src/lab01b/Essay.java
+++ src/lab01b/Essay.java
@@ -8,6 +8,47 @@ package lab01b;
  *
  * @author Edric
  */
-public class Essay {
+public class Essay extends GradedActivity {
+    protected double grammar;
+    protected double spelling;
+    protected double correctLength;
+    protected double content;
+
+    @Override
+    public void setScore(double score) {
+        score = grammar + spelling + correctLength + content;
+        super.setScore(score); 
+    }
+
+    public double getGrammar() {
+        return grammar;
+    }
     
+    public void setGrammar(double grammar) {
+        this.grammar = grammar;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public void setSpelling(double spelling) {
+        this.spelling = spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+
+    public void setCorrectLength(double correctLength) {
+        this.correctLength = correctLength;
+    }
+
+    public double getContent() {
+        return content;
+    }
+
+    public void setContent(double content) {
+        this.content = content;
+    }
 }
